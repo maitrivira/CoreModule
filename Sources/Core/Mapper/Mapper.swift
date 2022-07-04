6f15efedb401cedb18000fc17edbbc5fdc5fb137
Maitri Vira <maitrivira2@gmail.com>
2022-07-04 11:23:51 +0700

    add domain to entity mapper

--- Sources/Core/Mapper/Mapper.swift
+++ Sources/Core/Mapper/Mapper.swift
@@ -17,5 +17,6 @@ public protocol Mapper {
     func transformModelToEntity(request: Request?, response: Response) -> Entity
     func transformEntityToDomain(entity: Entity) -> Domain
     func transformResponseToDomain(request: Request?, response: Response) -> Domain
+    func transformDomainToEntity(domain: Domain) -> Entity
     
 }
