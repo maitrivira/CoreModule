272ba856e6763aafb8ef533d6f43f80d5c4ef902
Maitri Vira <maitrivira2@gmail.com>
2022-09-17 22:45:20 +0700

    edit mapper

--- Sources/Core/Mapper/Mapper.swift
+++ Sources/Core/Mapper/Mapper.swift
@@ -9,12 +9,13 @@ import Foundation
 
 public protocol Mapper {
     
+    associatedtype Request
     associatedtype Response
     associatedtype Entity
     associatedtype Domain
     
     func transformResponseToEntity(response: Response) -> Entity
-    func transformModelToEntity(response: Response) -> Entity
+    func transformModelToEntity(response: Request) -> Entity
     func transformEntityToDomain(entity: Entity) -> Domain
     func transformResponseToDomain(response: Response) -> Domain
     func transformDomainToEntity(domain: Domain) -> Entity
